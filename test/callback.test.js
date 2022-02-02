fefd340962e892b7606a64195c48f8580413cf69
HJ <eowr22@gmail.com>
2022-02-02 12:52:19 +0900

    feedback callback
    function - findIndex -> find
    third test - from first.json not second.json

--- test/callback.test.js
+++ test/callback.test.js
@@ -13,10 +13,11 @@ describe("Callback - fs.readFile를 콜백 패턴만 사용", () => {
     fs.readFile("assets/first.json", (err, firstData) => {
       fs.readFile("assets/second.json", (err, secondData) => {
         const first = JSON.parse(firstData);
-        const secondKey = first["second_key"];
         const second = JSON.parse(secondData);
-        const resultIndex = second.findIndex(data => data.key === secondKey);
-        const result = second[resultIndex]["hi"];
+
+        const secondKey = first["second_key"];
+        const resultObj = second.find((data) => data.key === secondKey);
+        const result = resultObj["hi"];
         expect(result).toBe("Second 방가방가");
         done();
       });
@@ -27,15 +28,21 @@ describe("Callback - fs.readFile를 콜백 패턴만 사용", () => {
     "assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트" +
       " 찾기",
     (done) => {
-      fs.readFile("assets/second.json", (err, secondData) => {
-        fs.readFile("assets/third.json", (err, thirdData) => {
-          const second = JSON.parse(secondData);
-          const thirdKey = second[0]["third_key"];
-          const third = JSON.parse(thirdData);
-          const resultIndex = third.findIndex(data => data.key === thirdKey);
-          const result = third[resultIndex]["hi"];
-          expect(result).toBe("Third 방가방가");
-          done();
+      fs.readFile("assets/first.json", (err, firstData) => {
+        fs.readFile("assets/second.json", (err, secondData) => {
+          fs.readFile("assets/third.json", (err, thirdData) => {
+            const first = JSON.parse(firstData);
+            const second = JSON.parse(secondData);
+            const third = JSON.parse(thirdData);
+
+            const secondKey = first["second_key"];
+            const secondObj = second.find((data) => data.key === secondKey);
+            const thirdKey = secondObj["third_key"];
+            const thirdObj = third.find((data) => data.key === thirdKey);
+            const result = thirdObj['hi'];
+            expect(result).toBe("Third 방가방가");
+            done();
+          });
         });
       });
     }
